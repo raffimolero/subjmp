7015f1f4f88a52b39429666ab1af18b1e28bcf51
raffimolero <raffimolero@gmail.com>
2023-08-31 22:32:03 +0800

    factored Program into struct

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,59 @@
-use std::{io::stdin, num::Wrapping as W};
+use std::{fmt::Display, io::stdin, num::Wrapping as W};
+
+#[derive(Clone, Copy, PartialEq, Eq)]
+struct Program {
+    mem: [W<i8>; 128],
+    acc: W<i8>,
+    ip: W<i8>,
+}
+
+impl Program {
+    fn new(mem: [u8; 128]) -> Self {
+        Self {
+            mem: mem.map(|x: u8| W(x as i8)),
+            acc: W(0),
+            ip: W(0),
+        }
+    }
+
+    fn step(&mut self) {
+        const HI: i8 = -0b_1000_0000;
+        let val = &mut self.mem[(self.ip.0 & !HI) as usize];
+        if val.0 & HI == 0 {
+            *val -= self.acc;
+            self.acc = *val;
+        } else if self.acc.0 < 0 {
+            self.ip += val.0 & !HI;
+        }
+    }
+}
+
+impl Display for Program {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        writeln!(f, "acc: {:>3}", self.acc)?;
+        writeln!(f, "ip: {:>3}", self.ip)?;
+        let mut i = W(0_i8);
+        'outer: for _ in 0..16 {
+            for _ in 0..8 {
+                let (l, r) = if i == self.ip { ('[', ']') } else { (' ', ' ') };
+                write!(f, "{l}{:>3}{r}", self.mem[i.0 as usize])?;
+
+                i += 1;
+                if i.0 == 126 {
+                    break 'outer;
+                }
+            }
+            writeln!(f)?;
+        }
+        writeln!(f)?;
+        writeln!(f, "Input: {:0>4b}", self.mem[126].0 & 0b_1111)?;
+        write!(f, "Output: {:0>8b}", self.mem[127])
+    }
+}
 
 fn main() {
-    let mut acc: W<i8> = W(0);
-    let mut ip = W(0);
     #[rustfmt::skip]
-    let mut mem: [W<i8>; 128] = [
+    let mem = [
           0,  0,  0,  0,  0,  0,  0,  0,
           0,  0,  0,  0,  0,  0,  0,  0,
           0,  0,  0,  0,  0,  0,  0,  0,
@@ -23,36 +72,13 @@ fn main() {
           0,  0,  0,  0,  0,  0,
         0b_0001_0000,     // &input = 126
         0b_1010_1110, // &output = 127
-    ].map(|x: u8| W(x as i8));
+    ];
+
+    let mut program = Program::new(mem);
 
     println!("Enter.");
     for _line in stdin().lines() {
-        const HI: i8 = -0b_1000_0000;
-        let val = &mut mem[(ip.0 & !HI) as usize];
-        if val.0 & HI == 0 {
-            *val -= acc;
-            acc = *val;
-        } else if acc.0 < 0 {
-            ip += val.0 & !HI;
-        }
-
-        println!("acc: {acc:>3}");
-        println!("ip: {ip:>3}");
-        let mut i = W(0_i8);
-        'outer: for _ in 0..16 {
-            for _ in 0..8 {
-                let (l, r) = if i == ip { ('[', ']') } else { (' ', ' ') };
-                print!("{l}{:>3}{r}", mem[i.0 as usize]);
-
-                i += 1;
-                if i.0 == 126 {
-                    break 'outer;
-                }
-            }
-            println!();
-        }
-        println!();
-        println!("Input: {:0>4b}", mem[126].0 & 0b_1111);
-        println!("Output: {:0>8b}", mem[127]);
+        program.step();
+        println!("{program}");
     }
 }
