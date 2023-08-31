d8306ef1d3c8c54e34b341eb26dc028a34b2672d
raffimolero <raffimolero@gmail.com>
2023-09-01 00:12:05 +0800

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
-use std::{fmt::Display, io::stdin, num::Wrapping as W};
+use std::{
+    fmt::Display,
+    io::{stdin, stdout, Write},
+    num::Wrapping as W,
+};
 
 #[derive(Clone, Copy, PartialEq, Eq)]
 struct Program {
@@ -37,7 +41,6 @@ impl Program {
         const HI: i8 = -0b_1000_0000;
         let cmd = self.mem[self.ip.0 as usize];
         let addr = (cmd.0 & !HI) as usize;
-        dbg!(addr);
         let val = &mut self.mem[addr];
 
         if cmd.0 & HI != 0 {
@@ -54,6 +57,22 @@ impl Program {
             const SENTINEL: i8 = 0b_0001_0000;
             *mmap = self.keys | SENTINEL;
             self.keys = 0;
+        }
+    }
+
+    fn prompt(&self) -> String {
+        println!("{self}");
+        print!("[01ab]> ");
+        stdout().flush().unwrap();
+        let line = stdin().lines().next().unwrap().unwrap();
+        println!();
+        line
+    }
+
+    fn run(&mut self) {
+        loop {
+            self.input(&self.prompt());
+            self.step();
         }
     }
 }
@@ -82,9 +101,14 @@ impl Display for Program {
 fn main() {
     #[rustfmt::skip]
     let mem = [
+    // LEGEND:
+    //    0x## => instruction
+    //    num  => used memory location
+    //      0  => unused
+    // ------------------------------------------------------
     //    _0    _1    _2    _3    _4    _5    _6    _7
     //    _8    _9    _a    _b    _c    _d    _e    _f
-        0xff, 0x86, 0x86, 0x85,128-5,   -1,    0,    0, // 8_
+        0xff, 0x86, 0x86, 0x85, 0x7b,   -1,   -0,    0, // 8_
            0,    0,    0,    0,    0,    0,    0,    0,
 
            0,    0,    0,    0,    0,    0,    0,    0, // 9_
@@ -107,16 +131,9 @@ fn main() {
 
            0,    0,    0,    0,    0,    0,    0,    0, // f_
            0,    0,    0,    0,    0,    0,
-        0b_0001_0000, // &input  = 126
-        0b_0000_0000, // &output = 127
+        0b_0001_0000, // &input  = 0xfe
+        0b_0000_0000, // &output = 0xff
     ];
 
-    let mut program = Program::new(0b_0000, 0x01, -1, mem);
-    println!("{program}");
-
-    for line in stdin().lines() {
-        program.input(&line.unwrap());
-        program.step();
-        println!("{program}");
-    }
+    Program::new(0b_0000, 0x00, -1, mem).run();
 }
